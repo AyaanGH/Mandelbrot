a08e6182049e26320f5d891eabf0187afb33f2f3
AyaanGH <{ID}+{username}@users.noreply.github.com>
2020-08-19 19:49:54 +0100

    Refactored Mandelbrot compute

--- main.cpp
+++ main.cpp
@@ -26,65 +26,93 @@ void SetWindow(int Width, int Height) {
     SetConsoleWindowInfo(Handle, TRUE, &Rect);            // Set Window Size 
 }
 
+void setConsoleWindow(int width,int height)
+{
+    HWND hwnd = GetConsoleWindow();
+    RECT rect = {500, 500, width, height};
+    MoveWindow(hwnd, rect.left, rect.top, rect.right-rect.left, rect.bottom-rect.top,TRUE);
+}
 
-float interpolate(int value, int leftMin, int leftMax, int rightMin, int rightMax) {
-    int leftSpan = leftMax - leftMin;
-    int rightSpan = rightMax - rightMin;
+
+float interpolate(float value, float leftMin, float leftMax, float rightMin, float rightMax) {
+    float leftSpan = leftMax - leftMin;
+    float rightSpan = rightMax - rightMin;
 
     float scale = float(value - leftMin) / (float) leftSpan;
 
     return (float) rightMin + (scale * (float) rightSpan);
 }
 
+int computeMandelbrot(float real_x, float imaginary_y) {
+    float z_real = real_x;
+    float z_imaginary = imaginary_y;
+
+    int iterations = 100;
+
+    for (int i = 0 ; i < iterations ; i++)
+    {
+
+        float z_real_squared = z_real * z_real;
+        float z_imaginary_squared = z_imaginary * z_imaginary;
+        if(z_real_squared + z_imaginary_squared > 16.0)
+        {
+            return i;
+        }
+        z_real = z_real_squared - z_imaginary_squared + real_x ;
+        z_imaginary = 2.0f * z_real *z_imaginary + imaginary_y;
+
+    }
+    return iterations;
+}
+
 int main() {
 
     const int height = 90;
     const int width = 200;
-    SetWindow(width, height);
-    setCursorPos(0, 0);
+//    SetWindow(width, height);
 
+//    setConsoleWindow(1920,1080);
+    setCursorPos(0, 0);
 
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width - 1; ++x) {
 
-            float b = interpolate(y, 0, height, -2, 2);
-            float a = interpolate(x, 0, width, -2, 2);
+            float real_x = interpolate(x, 0, width, -2, 2);
+            float imaginary_y = interpolate(y, 0, height, -2, 2);
 
-            float ca = a;
-            float cb = b;
+            int tending_val = computeMandelbrot(real_x, imaginary_y);
 
-            int n = 0;
-
-
-            while (n < 100) {
-                //a^2 - b^2 == aabb
-
-                float aabb = a * a - b * b;
-
-                float ab = 2 * a * b;
-
-                a = aabb + ca;
-
-                b = ab + cb;
-
-                if (std::abs(a + b) > 16) {
-                    break;
-                }
-                ++n;
-
-            }
-
-
-            if (n == 100) {
-                std::cout << "O";
-            } else {
+            if (tending_val == 100) {
                 std::cout << " ";
+            } else {
+                std::cout << ".";
             }
 
         }
+
+//            float ca = a;
+//            float cb = b;
+//
+//            int n = 0;
+//            while (n < 100) {
+//                float aabb = a * a - b * b;
+//                float ab = 2 * a * b;
+//                a = aabb + ca;
+//                b = ab + cb;
+//
+//                if (std::abs(a + b) > 16) {
+//                    break;
+//                }
+//                ++n;
+//
+//            }
+//
+//
+//        }
 
         std::cout << '\n';
     }
 //    setCursorPos(0, 0);
     return 0;
 }
+
