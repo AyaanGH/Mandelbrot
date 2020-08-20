43b2fe2aee3e05efa9b46c11384949d94dc65f5b
AyaanGH <{ID}+{username}@users.noreply.github.com>
2020-08-20 14:54:15 +0100

    Added colouring to terminal

--- main.cpp
+++ main.cpp
@@ -23,7 +23,8 @@ void SetWindow(int Width, int Height) {
 
     HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);      // Get Handle 
     SetConsoleScreenBufferSize(Handle, coord);            // Set Buffer Size 
-    SetConsoleWindowInfo(Handle, TRUE, &Rect);            // Set Window Size 
+    SetConsoleWindowInfo(Handle, TRUE, &Rect);            // Set Window Size
+    SetConsoleMode(Handle,0x0200);
 }
 
 void setConsoleWindow(int width, int height) {
@@ -64,29 +65,112 @@ int computeMandelbrot(float real_x, float imaginary_y) {
 
 int main() {
 
-    const int height = 90;
-    const int width = 200;
+    const int height = 80;
+    const int width = 150;
 
+    SetWindow(width,height+1);
     setCursorPos(0, 0);
+    HANDLE h = GetStdHandle( STD_OUTPUT_HANDLE );
 
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width - 1; ++x) {
 
-            float real_x = interpolate(x, 0, width, -2, 2);
-            float imaginary_y = interpolate(y, 0, height, -2, 2);
 
-            int tending_val = computeMandelbrot(real_x, imaginary_y);
+    for (float zoom = 1; zoom < 100 ; zoom *= 1.04) {
 
-            if (tending_val == 100) {
-                std::cout << " ";
-            } else {
-                std::cout << ".";
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width - 1; ++x) {
+
+                float real_x = interpolate(x, 0, width, -2+zoom, 2);
+                float imaginary_y = interpolate(y, 0, height, -2, 2-zoom);
+
+                int tending_val = computeMandelbrot(real_x, imaginary_y);
+
+                if (tending_val == 100) {
+                    SetConsoleTextAttribute(h, 0);
+                    printf( " ");
+
+                }
+
+                else {
+                    const char *symbol = "#";
+                    if (tending_val > 90) {
+                        SetConsoleTextAttribute(h, 4);
+                        printf("%s", symbol);
+                    }
+                    else if (tending_val > 70) {
+                        SetConsoleTextAttribute(h, 12);
+                        printf("%s", symbol);
+                    }
+                    else if (tending_val > 50) {
+                        SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN);
+                        printf("%s", symbol);
+                    }
+                    else if (tending_val > 30) {
+                        SetConsoleTextAttribute(h, 14);
+                        printf("%s", symbol);
+                    }
+
+                    else if (tending_val > 20) {
+                        SetConsoleTextAttribute(h, 10);
+                        printf("%s", symbol);
+                    }
+                    else if (tending_val > 10) {
+                        SetConsoleTextAttribute(h, 2);
+                        printf("%s", symbol);
+                    }
+
+                    else if (tending_val > 5) {
+                        SetConsoleTextAttribute(h, 11);
+                        printf("%s", symbol);
+                    }
+                    else if (tending_val > 4) {
+                        SetConsoleTextAttribute(h, 3);
+                        printf("%s", symbol);
+                    }
+
+                    else if (tending_val > 3) {
+                        SetConsoleTextAttribute(h, 9);
+                        printf("%s", symbol);
+                    }
+                    else if (tending_val > 2) {
+                        SetConsoleTextAttribute(h, 1);
+                        printf("%s", symbol);
+                    }
+                    else if (tending_val > 1) {
+                        SetConsoleTextAttribute(h, 5);
+                        printf("%s", symbol);
+                    }
+
+                    else
+                    {
+                        SetConsoleTextAttribute(h, 13);
+                        printf("%s", symbol);
+                    }
+                }
+
+
+
+
+
+
+
+
+//                else if (tending_val > 2) {std::cout << blue << normal;}
+//                else if (tending_val > 1) {std::cout << magenta << normal;}
+//                else {std::cout << l_magenta << normal;}
+//
+//                std::cout << "\033[0m ";
+//                if (tending_val == 100) {
+//                    std::cout << "\033[22;32mA";
+//                } else {
+//                    std::cout << " ";
+//                }
+
             }
 
+
+            std::cout << '\n';
         }
-
-
-        std::cout << '\n';
+        std::system("cls");
     }
 
     return 0;
